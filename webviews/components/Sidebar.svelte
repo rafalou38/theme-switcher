7853d4e6e1237bf63d78f1d1e6244e9eaa10dd7c
Rafael <rafael.martinezcalvo@orange.fr>
2021-05-31 21:04:13 +0200

    add favorite fonctionality

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,68 +1,105 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { themes } from "./stores";
-	export let vscode: WebviewApi;
+  import { onMount } from "svelte";
+  import { flip } from "svelte/animate";
 
-	let favorites: Set<string> = new Set(['Abyss', "Default Light+"])
+  import { themes } from "./stores";
 
-	onMount(()=>{
-		vscode.postMessage("getThemes")
-	})
+  import FillStar from "../icons/star-solid.svg";
+  import EmptyStar from "../icons/star-regular.svg";
+  export let vscode: WebviewApi;
+
+  let favorites: Set<string> = new Set(
+    JSON.parse(localStorage.getItem("favorites") || "[]")
+  );
+
+  $: {
+    localStorage.setItem("favorites", JSON.stringify(Array.from(favorites)));
+  }
+  onMount(() => {
+    vscode.postMessage("getThemes");
+  });
 </script>
 
-<div class="list--title">favorites</div>
+<div class="list__title">favorites</div>
 <ul class="list">
-	{#each $themes.filter(e=>favorites.has(e.id)) as theme}
-		 <li class="list--item">
-			 {theme.name}
-			</li>
-	{/each}
+  {#each $themes.filter((e) => favorites.has(e.id)) as theme (theme.id)}
+    <li class="list__item" animate:flip={{ duration: 200 }}>
+      {theme.name}
+      <button
+        class="list__item__star"
+        on:click={() => {
+          favorites.delete(theme.id);
+          favorites = favorites;
+        }}><FillStar /></button
+      >
+    </li>
+  {/each}
 </ul>
 
-<div class="list--title">all themes</div>
+<div class="list__title">all themes</div>
 <ul class="list">
-	{#each $themes.filter(e=>!favorites.has(e.id)) as theme}
-		 <li class="list--item">
-			 {theme.name}
-			</li>
-	{/each}
+  {#each $themes.filter((e) => !favorites.has(e.id)) as theme (theme.id)}
+    <li class="list__item" animate:flip={{ duration: 200 }}>
+      {theme.name}
+      <button
+        class="list__item__star"
+        on:click={() => {
+          favorites = favorites.add(theme.id);
+        }}
+        ><EmptyStar />
+      </button>
+    </li>
+  {/each}
 </ul>
-
 
 <style lang="scss">
-	.list{
-		list-style: none;
-	}
-	.list--title{
-		background: var(--vscode-dropdown-listBackground);
-		color: var(--vscode-dropdown-foreground);
-	}
-	.list--item{
-		position: relative;
-		z-index: 1;
+  .list {
+    list-style: none;
+  }
+  .list__title {
+    background: var(--vscode-dropdown-listBackground);
+    color: var(--vscode-dropdown-foreground);
+  }
+  .list__item {
+    position: relative;
+    z-index: 1;
 
-		height: 2em;
-		padding: .5em;
+    height: 2em;
+    padding: 0.5em;
 
-		user-select: none;
-		cursor: pointer;
+    user-select: none;
+    cursor: pointer;
 
-		display: flex;
-		align-items: center;
+    display: flex;
+    align-items: center;
 
-		font-weight: 500;
-		&::before{
-			content: "";
-			background: var(--vscode-editor-background);
-			position: absolute;
-			z-index: -1;
-			top: 0;
-			left: 0;
-			right: 0;
-			bottom: 0;
-		}
-		&:hover::before{
-			filter: brightness(120%);
-		}
-	}
+    font-weight: 500;
+    &::before {
+      content: "";
+      background: var(--vscode-editor-background);
+      position: absolute;
+      z-index: -1;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+    &:hover::before,
+    &--current::before {
+      filter: brightness(120%);
+    }
+  }
+  .list__item__star {
+    display: block;
+    position: absolute;
+    right: 0;
+
+    width: 2em;
+
+    background: none;
+
+    :global(svg) {
+      color: var(--vscode-foreground);
+    }
+  }
 </style>
