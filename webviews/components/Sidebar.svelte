07d58e741632886be9a89ca44c8aa7e0605fffd9
Rafael <rafael.martinezcalvo@orange.fr>
2021-05-31 19:32:30 +0200

    add basic theme list

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -4,18 +4,66 @@ import { themes } from "./stores";
 
 	export let vscode: WebviewApi;
 
+	let favorites: Set<string> = new Set(['Abyss', "Default Light+"])
+
 	onMount(()=>{
 		vscode.postMessage("getThemes")
 	})
 </script>
 
-<ul>
-	{#each $themes as theme}
-		 <li>{theme.name}</li>
+<div class="list--title">favorites</div>
+<ul class="list">
+	{#each $themes.filter(e=>favorites.has(e.id)) as theme}
+		 <li class="list--item">
+			 {theme.name}
+			</li>
+	{/each}
+</ul>
+
+<div class="list--title">all themes</div>
+<ul class="list">
+	{#each $themes.filter(e=>!favorites.has(e.id)) as theme}
+		 <li class="list--item">
+			 {theme.name}
+			</li>
 	{/each}
 </ul>
 
 
 <style lang="scss">
+	.list{
+		list-style: none;
+	}
+	.list--title{
+		background: var(--vscode-dropdown-listBackground);
+		color: var(--vscode-dropdown-foreground);
+	}
+	.list--item{
+		position: relative;
+		z-index: 1;
 
+		height: 2em;
+		padding: .5em;
+
+		user-select: none;
+		cursor: pointer;
+
+		display: flex;
+		align-items: center;
+
+		font-weight: 500;
+		&::before{
+			content: "";
+			background: var(--vscode-editor-background);
+			position: absolute;
+			z-index: -1;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+		}
+		&:hover::before{
+			filter: brightness(120%);
+		}
+	}
 </style>
